ae22b5bd2f599e49a7edecef4f823bf6969a9b84
javierforumSS <javierforum@gmail.com>
2025-12-11 19:11:47 +0100

    Update config.js

--- config.js
+++ config.js
@@ -1,5 +1,3 @@
-import { createClient } from 'https://cdn.jsdelivr.net/npm/@supabase/supabase-js/+esm'
-  
 const SUPABASE_URL = "https://gqhfimpbjocjshjlcdmy.supabase.co";
 const SUPABASE_ANON_KEY = "D222$YQ4s6HWew!"; // la que me diste
 const supabase = supabase.createClient(SUPABASE_URL, SUPABASE_ANON_KEY);
