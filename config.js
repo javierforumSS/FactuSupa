916a249c0f259c6043269f10ba917e4031ce6995
javierforumSS <javierforum@gmail.com>
2025-12-11 18:09:31 +0100

    Create config.js

---
+++ config.js
@@ -0,0 +1,3 @@
+const SUPABASE_URL = "https://tu-proyecto.supabase.co";
+const SUPABASE_ANON_KEY = "D222$YQ4s6HWew!"; // la que me diste
+const supabase = supabase.createClient(SUPABASE_URL, SUPABASE_ANON_KEY);
