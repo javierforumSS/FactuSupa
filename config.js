e14692e0d3fa5efe0038d379bd5dcbdf6ff9f69a
javierforumSS <javierforum@gmail.com>
2025-12-11 19:28:09 +0100

    Update config.js

--- config.js
+++ config.js
@@ -1,3 +1,5 @@
 const SUPABASE_URL = "https://gqhfimpbjocjshjlcdmy.supabase.co";
 const SUPABASE_ANON_KEY = "D222$YQ4s6HWew!"; // la que me diste
-const supabase = supabase.createClient(SUPABASE_URL, SUPABASE_ANON_KEY);
+
+// Cliente Supabase global
+window.supabase = supabase.createClient(SUPABASE_URL, SUPABASE_ANON_KEY);
